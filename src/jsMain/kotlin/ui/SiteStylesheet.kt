b60e7f8a1e060b942bf8147aa582adc705c98ba4
Winter Snow <unknown@si.deep>
2022-08-31 00:17:43 +0300

    Fix style

--- src/jsMain/kotlin/ui/SiteStylesheet.kt
+++ src/jsMain/kotlin/ui/SiteStylesheet.kt
@@ -3,6 +3,7 @@ package ui
 import org.jetbrains.compose.web.css.Color
 import org.jetbrains.compose.web.css.StyleSheet
 import org.jetbrains.compose.web.css.color
+import org.jetbrains.compose.web.css.fontFamily
 
 object SiteStylesheet : StyleSheet() {
     const val primaryColor = "#FB77FF"
@@ -10,6 +11,9 @@ object SiteStylesheet : StyleSheet() {
     init {
         "a" style {
             color(Color.black)
+        }
+        "div" style {
+            fontFamily("segoe-ui")
         }
     }
 }
